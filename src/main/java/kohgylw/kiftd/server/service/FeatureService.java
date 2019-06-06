5e760876c3e120b296342b2995c903a66869b56e
woochiyan <185441633@qq.com>
2019-06-06 11:09:59 +0800

    功能增删改查

--- src/main/java/kohgylw/kiftd/server/service/FeatureService.java
+++ src/main/java/kohgylw/kiftd/server/service/FeatureService.java
@@ -4,4 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 public interface FeatureService {
     String getFeatureViewToJson(final HttpServletRequest request);
+    String newFeature(final HttpServletRequest request);
+    String deleteFeature(final HttpServletRequest request);
+    String renameFeature(final HttpServletRequest request);
 }
